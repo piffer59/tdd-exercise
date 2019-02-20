b18a821b40f440e9511c8b2fe5bba40fca0b4fd3
Heather Izumi <hkizumi@gmail.com>
2019-02-20 14:00:39 -0800

    Added blackjack code

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,12 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"]
+# face_cards = ["King", "Queen", "Jack"]
 
 def blackjack_score(hand)
-
+  hand.sample(2).reduce(:+)
 end
+
+blackjack_score(VALID_CARDS)
+
+# print hand
